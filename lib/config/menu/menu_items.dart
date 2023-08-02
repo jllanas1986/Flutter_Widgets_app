a2f209cffa38bdbb756afeb1e3c90fc51b76454a
Jose Llanas <josellanasmota86@gmail.com>
2023-08-02 12:06:59 +0200

    animated container

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -44,4 +44,11 @@ const appMenuItems = <MenuItem>[
     icon: Icons.info_outline
   ),
 
+  MenuItem(
+    title: 'Animated container',
+    subTitle: 'Statefull widget animado',
+    link: '/animated',
+    icon: Icons.check_box_outline_blank_rounded
+  ),
+
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -37,5 +37,11 @@ final appRouter = GoRouter(
       builder: (context, state) => const SnackbarScreen(),
     ),
 
+    GoRoute(
+      path: '/animated',
+      name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
+    ),
+
   ],
 );
