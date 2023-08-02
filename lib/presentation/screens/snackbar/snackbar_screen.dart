d5da034cf66f6d10de649fb08ccd8543db227bd3
Jose Llanas <josellanasmota86@gmail.com>
2023-08-02 11:50:09 +0200

    dialogues and licences

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -6,7 +7,6 @@ class SnackbarScreen extends StatelessWidget {
   const SnackbarScreen({super.key});
 
   void showCustomSnackbar(BuildContext context) {
-
     ScaffoldMessenger.of(context).clearSnackBars();
 
     final snackBack = SnackBar(
@@ -18,12 +18,46 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackBack);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Estas Seguro?'),
+        content: const Text(
+            'Proident ad fugiat elit mollit labore pariatur consequat ut et ad do. Cupidatat consequat Lorem est cillum quis labore. In sint ut excepteur sint anim mollit irure irure. Tempor est velit reprehenderit commodo elit aute Lorem ex excepteur.'),
+        actions: [
+          TextButton(onPressed: () => context.pop(), child: const Text('Cancelar')),
+
+          FilledButton(onPressed: () => context.pop(), child: const Text('Aceptar')),
+        ]
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y Diálogos'),
       ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(context: context, children: [
+                  const Text(
+                      'VSit veniam minim occaecat nulla adipisicing sit. Labore mollit occaecat ad consequat. Duis elit qui fugiat pariatur magna cupidatat ea aliqua. Laboris nostrud amet officia anim do id culpa.eniam sunt nostrud enim cupidatat mollit et dolore do est.')
+                ]);
+              },
+              child: const Text('Licencias usadas')),
+          FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar dialogo'))
+        ],
+      )),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
         icon: const Icon(Icons.remove_red_eye_outlined),
