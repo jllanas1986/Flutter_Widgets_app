2f0135c8779fafec566a52e770e48767dd70c55a
Jose Llanas <josellanasmota86@gmail.com>
2023-08-02 20:32:03 +0200

    images added

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -6,55 +6,47 @@ class MenuItem {
   final String link;
   final IconData icon;
 
-  const MenuItem({
-    required this.title,
-    required this.subTitle,
-    required this.link,
-    required this.icon
-  });
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
 }
 
 const appMenuItems = <MenuItem>[
-
   MenuItem(
-    title: 'Botones',
-    subTitle: 'Varios botones en Flutter',
-    link: '/buttons',
-    icon: Icons.smart_button_outlined
-  ),
-
+      title: 'Botones',
+      subTitle: 'Varios botones en Flutter',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined),
   MenuItem(
-    title: 'Tarjetas',
-    subTitle: 'Un contenedor estilizado',
-    link: '/cards',
-    icon: Icons.credit_card
-  ),
-
+      title: 'Tarjetas',
+      subTitle: 'Un contenedor estilizado',
+      link: '/cards',
+      icon: Icons.credit_card),
   MenuItem(
-    title: 'Progress Indicators',
-    subTitle: 'Generales y controlados',
-    link: '/progress',
-    icon: Icons.refresh_rounded
-  ),
-
+      title: 'Progress Indicators',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded),
   MenuItem(
-    title: 'Snackbars y diálogos',
-    subTitle: 'Indicadores en pantalla',
-    link: '/snackbars',
-    icon: Icons.info_outline
-  ),
-
+      title: 'Snackbars y diálogos',
+      subTitle: 'Indicadores en pantalla',
+      link: '/snackbars',
+      icon: Icons.info_outline),
   MenuItem(
-    title: 'Animated container',
-    subTitle: 'Statefull widget animado',
-    link: '/animated',
-    icon: Icons.check_box_outline_blank_rounded
-  ),
-
+      title: 'Animated container',
+      subTitle: 'Statefull widget animado',
+      link: '/animated',
+      icon: Icons.check_box_outline_blank_rounded),
   MenuItem(
-    title: 'UI Controls + Tiles',
-    subTitle: 'Una serie de controles de Flutter',
-    link: '/ui-controls',
-    icon: Icons.car_rental_outlined
-  ),
+      title: 'UI Controls + Tiles',
+      subTitle: 'Una serie de controles de Flutter',
+      link: '/ui-controls',
+      icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: 'Introducción a la aplicación',
+      subTitle: 'Pequeño tutorial introductorio',
+      link: '/tutorial',
+      icon: Icons.accessible_rounded),
 ];
