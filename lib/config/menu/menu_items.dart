12fea8e48e305f2af849ae18d985d50358e2bea0
Jose Llanas <josellanasmota86@gmail.com>
2023-08-02 10:17:35 +0200

    progressIndicator implemented

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -30,4 +30,11 @@ const appMenuItems = <MenuItem>[
     icon: Icons.credit_card
   ),
 
+  MenuItem(
+    title: 'ProgressIndicators',
+    subTitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded
+  ),
+
 ];
--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
 class ProgressScreen extends StatelessWidget {
-
   static const name = 'progress_screen';
-  
+
   const ProgressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress Indicators'),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Text('Circular progress indicator'),
+          SizedBox(height: 30),
+          CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45),
+
+          SizedBox(height: 20),
+          Text('Circular indicator controled'),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator()
+          
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
