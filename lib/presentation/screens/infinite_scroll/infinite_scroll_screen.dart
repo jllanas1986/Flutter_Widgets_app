b8f42a3c269633bd57aec9b81fabbabe567da22f
Jose Llanas <josellanasmota86@gmail.com>
2023-08-03 20:42:59 +0200

    infiniteScroll infinite images

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -12,6 +12,49 @@ class InfiniteScrollScreen extends StatefulWidget {
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   List<int> imagesIds = [1, 2, 3, 4, 5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        //load next page
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    //TO DO REVISAR SI ESTA MONTADO EL COMPONENTE/WIDGET
+    if (isMounted) return;
+    setState(() {});
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +65,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
+          controller: scrollController,
           itemCount: imagesIds.length,
           itemBuilder: (context, index) {
             return FadeInImage(
@@ -29,17 +73,15 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
               width: double.infinity,
               height: 300,
               placeholder: const AssetImage('assets/images/jar-loading.gif'),
-              image:
-                  NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300'),
+              image: NetworkImage(
+                  'https://picsum.photos/id/${imagesIds[index]}/500/300'),
             );
           },
         ),
       ),
-
       floatingActionButton: FloatingActionButton(
-        onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back_ios_new_outlined)
-      ),
+          onPressed: () => context.pop(),
+          child: const Icon(Icons.arrow_back_ios_new_outlined)),
     );
   }
 }
