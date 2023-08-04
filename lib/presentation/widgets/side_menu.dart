3f3cde1702ebc732ac7ee6b0509edf34278c4645
Jose Llanas <josellanasmota86@gmail.com>
2023-08-04 11:51:32 +0200

    navigate since menu

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widget_app/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
-  const SideMenu({super.key});
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const SideMenu({super.key, required this.scaffoldKey});
 
   @override
   State<SideMenu> createState() => _SideMenuState();
@@ -13,49 +16,42 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return NavigationDrawer(
         selectedIndex: navDrawerIndex,
         onDestinationSelected: (value) {
           setState(() {
             navDrawerIndex = value;
           });
+
+          final menuItem = appMenuItems[value];
+          context.push(menuItem.link);
+          widget.scaffoldKey.currentState?.closeDrawer();
         },
         children: [
-
           Padding(
             padding: const EdgeInsets.fromLTRB(28, 20, 16, 10),
             child: Text('Main'),
           ),
-
-          ...appMenuItems
-            .sublist(0,3)         
-            .map((item) => NavigationDrawerDestination(
-              icon: Icon( item.icon ), 
-              label: Text(item.title),
-            ),
-          ),
-
+          ...appMenuItems.sublist(0, 3).map(
+                (item) => NavigationDrawerDestination(
+                  icon: Icon(item.icon),
+                  label: Text(item.title),
+                ),
+              ),
           const Padding(
             padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
             child: Divider(),
           ),
-
           const Padding(
             padding: EdgeInsets.fromLTRB(28, 20, 16, 10),
             child: Text('More options'),
           ),
-
-          ...appMenuItems
-            .sublist(0,3)         
-            .map((item) => NavigationDrawerDestination(
-              icon: Icon( item.icon ), 
-              label: Text(item.title),
-            ),
-          ),
-        ]
-      );
+          ...appMenuItems.sublist(0, 3).map(
+                (item) => NavigationDrawerDestination(
+                  icon: Icon(item.icon),
+                  label: Text(item.title),
+                ),
+              ),
+        ]);
   }
 }
